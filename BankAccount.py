9a98341ebe91e968c56f898d43279acb331ac016
Shahir Ali <shahirsheikali@gmail.com>
2020-11-09 18:50:54 -0800

    push

---
+++ BankAccount.py
@@ -0,0 +1,8 @@
+#Intaialze the class
+#Class attributes
+class BankAccount:
+    def __init__(self, fullname, accountnumber, routingnumber, accountbalance):
+      self.fullname = fullname
+      self.accountnumber = accountnumber
+      self.routingnumber = routingnumber 
+      self.accountbalance = accountbalance
